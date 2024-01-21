8f87e1ccfc54e7032a5373753965f930f4177493
alexxey12060 <154739411+alexxey12060@users.noreply.github.com>
2024-01-21 15:40:32 +0300

    + движения

--- main.py
+++ main.py
@@ -23,21 +23,21 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def update_r(self):
         keys = key.get_pressed()
-        if keys[k_w] and self.rect.y>5:
+        if keys[K_w] and self.rect.y>5:
             self.rect.y -= self.speed
-        if keys[k_s] and self.rect.y< win_height - 80:
+        if keys[K_s] and self.rect.y< win_height - 80:
             self.rect.y += self.speed
 
     def update_l(self):
         keys = key.get_pressed()
-        if keys[k_UP] and self.rect.y>5:
+        if keys[K_UP] and self.rect.y>5:
             self.rect.y -= self.speed
-        if keys[k_DOWN] and self.rect.y< win_height - 80:
+        if keys[K_DOWN] and self.rect.y< win_height - 80:
             self.rect.y += self.speed
 
-rocket2 = Player('vfibyf vfplf.png', 4, 30,200,100,150)
+rocket2 = Player('pngegg.png', 4, 30,200,100,200)
 rocket1 = Player('bmw cdth[e.png',4, 500,200,100,200)
-ball = Player("pngwing.com.png",4, 200, 300 , 70,70)
+ball = Player("watanabe-rs-r15-16-8j-et25-4-bronze-1.png",4, 200, 300 , 60,60)
 
 
 
@@ -47,33 +47,44 @@ FPS = 60
 finish = False
 game = True
 
+speed_x = 3
+speed_y = 3
+
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('kanjoo1 проиграл', True, (180, 0 ,0))
+lose2 = font.render('kanjoo2 проиграл', True, (180, 0 ,0))
 while game:
     for e in event.get():
         if e.type == QUIT:
             game= False
 
     if finish != True:
+        window.fill(back)
+        rocket1.update_l()
+        rocket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        if sprite.collide_rect(rocket1, ball) or sprite.collide_rect(rocket2, ball):
+            speed_x *= -1
+        if ball.rect.y < 0 or ball.rect.y > 450:
+            speed_y *= -1
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1,(200,200))
+            game_over = True
+
+        if ball.rect.x > 600:
+            finish = True
+            window.blit(lose1,(200,200))
+            game_over = True
         rocket1.reset()
         rocket2.reset()
         ball.reset()
 
     display.update()
     clock.tick(FPS)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
